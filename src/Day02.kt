f2f6566823573ee0c60bd0a804a03489e9fca99d
Dominik Liebler <dominik.liebler@yazio.com>
2021-12-02 13:04:23 +0100

    refactored day 2 (thanks to Paul for the hint!)

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,5 +1,27 @@
-private data class SimpleNavigation(val depth: Int = 0, val horizontal: Int = 0)
-private data class AdvancedNavigation(val depth: Int = 0, val horizontal: Int = 0, val aim: Int = 0)
+private data class SimpleNavigation(val depth: Int = 0, val horizontal: Int = 0) {
+    fun withDepth(depth: Int) = SimpleNavigation(horizontal = this.horizontal, depth = depth)
+    fun withHorizontal(horizontal: Int) = SimpleNavigation(horizontal = horizontal, depth = this.depth)
+}
+
+private data class AdvancedNavigation(val depth: Int = 0, val horizontal: Int = 0, val aim: Int = 0) {
+    fun forward(value: Int) = AdvancedNavigation(
+        depth = this.depth + this.aim * value,
+        horizontal = this.horizontal + value,
+        aim = this.aim
+    )
+
+    fun down(value: Int) = AdvancedNavigation(
+        depth = this.depth,
+        horizontal = this.horizontal,
+        aim = this.aim + value
+    )
+
+    fun up(value: Int) = AdvancedNavigation(
+        depth = this.depth,
+        horizontal = this.horizontal,
+        aim = this.aim - value
+    )
+}
 
 fun main() {
     fun part1(input: List<String>): Int {
@@ -7,9 +29,9 @@ fun main() {
             val (command, value) = element.split(" ")
 
             when (command) {
-                "forward" -> SimpleNavigation(out.depth, out.horizontal + value.toInt())
-                "down" -> SimpleNavigation(out.depth + value.toInt(), out.horizontal)
-                else -> SimpleNavigation(out.depth - value.toInt(), out.horizontal)
+                "forward" -> out.withHorizontal(out.horizontal + value.toInt())
+                "down" -> out.withDepth(out.depth + value.toInt())
+                else -> out.withDepth(out.depth - value.toInt())
             }
         }
 
@@ -21,9 +43,9 @@ fun main() {
             val (command, value) = element.split(" ")
 
             when (command) {
-                "forward" -> AdvancedNavigation(out.depth + out.aim * value.toInt(), out.horizontal + value.toInt(), out.aim)
-                "down" -> AdvancedNavigation(out.depth, out.horizontal, out.aim + value.toInt())
-                else -> AdvancedNavigation(out.depth, out.horizontal, out.aim -  value.toInt())
+                "forward" -> out.forward(value.toInt())
+                "down" -> out.down(value.toInt())
+                else -> out.up(value.toInt())
             }
         }
 
